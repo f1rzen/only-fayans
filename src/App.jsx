05ffec56af72d0b84e6430f4f30db3c980b63f79
İlkay Şafak Baytar <ilkay@duck.com>
2023-09-05 10:58:23 +0300

    adding stats and some static images

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Stats from './components/Stats'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+      <div className="hero bg-base-200">
+  <div className="hero-content flex-col lg:flex-row">
+    <img src="src/assets/images/fayansci.png" className="max-w-xs lg:max-w-sm rounded-lg shadow-2xl" />
     <div>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+      <h1 className="text-5xl font-bold">Evinizin Döşemelerine Zarafet Katın</h1>
+      <p className="py-6">Fayansın özgün dokusu ve dayanıklılığı ile yaşam alanlarınızı yeniden keşfedin. Evinizi güzelleştirmek için mükemmel fayans seçenekleri sunuyoruz. Kalite, estetik ve dayanıklılığı bir araya getirerek, hayalinizdeki yaşam alanını oluşturmanıza yardımcı oluyoruz.</p>
+      <button className="btn btn-primary">Hemen Keşfet!</button>
     </div>
   </div>
 </div>
+    <h2 className='text-4xl font-bold wrapper'>Müşteri Memnuniyetimiz</h2>
+    <Stats/>
     </>
   )
 }
